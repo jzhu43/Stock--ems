969a4c65def652abfc7a830708ec337138f60844
Jason Zhu <jzhu43@ucmerced.edu>
2022-01-06 14:30:11 -0800

    added express validator for user accounts

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-//@route    GET api/users
-//@descTest route
+//@route    POST api/users
+//@descTest register users
 //@access   Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Must be a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
